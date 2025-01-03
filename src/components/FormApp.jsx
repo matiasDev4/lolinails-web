23bf1c9c4abd3960412df8fcfd255a7a9c4e02ae
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-01-03 02:43:03 -0300

    modificacion FormApp

--- src/components/FormApp.jsx
+++ src/components/FormApp.jsx
@@ -3,19 +3,29 @@ export const FormApp = () =>{
         <>
         <div className="bg-black my-5 h-auto py-[25px] px-[50px] rounded-lg text-white">
             <form action="">
-                <div className="flex flex-col w-full gap-y-5">
-                    <div className="flex flex-col gap-y-[10px]">
+                <div className="flex flex-col w-full gap-y-5 [&>div>input]:rounded-[6px]">
+                    <div className="flex flex-col gap-y-[10px] ">
                         <span>Nombre</span>
-                        <input type="text" name="name" id="name" />
+                        <input type="text" name="name" id="name" 
+                        className="py-[8px] text-black px-[10px] outline-purple-800 bg-gray-400"/>
                     </div>
                     <div className="flex flex-col gap-y-[10px]">
-                        <span>DNI</span>
-                        <input type="text" name="dni" id="dni" />
+                        <span>Email</span>
+                        <input type="text" name="dni" id="dni" 
+                        className="py-[8px] text-black px-[10px] outline-purple-800 bg-gray-400"/>
                     </div>
                     <div className="flex flex-col gap-y-[10px]">
-                        <span>DNI</span>
-                        <select name="fecha" id="fecha">
+                        <span>Seleccionar fecha</span>
+                        <select name="fecha" id="fecha" 
+                        className="py-[8px] text-black px-[10px] outline-purple-800 bg-gray-400">
                             <option value="val1">20/01/2025</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col gap-y-[10px]">
+                        <span>Seleccionar horario</span>
+                        <select name="fecha" id="fecha" 
+                        className="py-[8px] text-black px-[10px] outline-purple-800 bg-gray-400">
+                            <option value="val1">10:00</option>
                         </select>
                     </div>
 
