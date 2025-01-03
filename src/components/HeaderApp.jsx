fe19405adf84c141d3ccceb42828eb33f409290e
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-01-03 02:09:32 -0300

    modificacion headerapp padding y heigth

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -3,8 +3,8 @@
 export const HeaderApp = () =>{
     return (
         <>
-        <header className="bg-black h-[18vh] rounded-lg text-white shadow-[0px_0px_20px_black]">
-            <div className="flex flex-col justify-center items-center h-full">
+        <header className="bg-black min-h-[20vh] rounded-lg text-white shadow-[0px_0px_20px_black]">
+            <div className="flex flex-col justify-center items-center min-h-[20vh] py-[10px]">
                 <img src="./src/assets/images/logo.png" alt="profile" 
                 className="w-[100px] h-[100px] rounded-full"/>
                 <span className="text-[30px] my-[2px] font-gotic">Agenda tu turno</span>
